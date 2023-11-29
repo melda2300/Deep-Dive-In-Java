159cb516b880521cec1b0c279bcc99f3c068a776
melda <146105556+melda2300@users.noreply.github.com>
2023-11-29 15:08:59 +0300

    Add files via upload

---
+++ Başlangıc.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Başlangıc {
+    public static void main(String[] args) {
+int sayı=1; // tam sayı
+double sayılar= 1.23333; // ondalık sayılar
+boolean d_y=true; // doğru mu yanlış mı
+char harf='a';// tek harf
+        System.out.println("tam syı: "+sayı);
+        System.out.println("ondalıklı sayı: "+sayılar);
+        System.out.println("doğru mu yanlış mı onu döndürüyor: "+d_y);
+        System.out.println("tek bir harf "+harf);
+        }
+    }
